e3c0b88bd70e5853b576df46271eed3bba1c5f30
Alberto López Sánchez <alopez@cells.es>
2021-02-17 12:38:23 +0100

    adds more features

--- omicron_laser/core.py
+++ omicron_laser/core.py
@@ -24,9 +24,13 @@ It can receive an asynchronous connection object. Example::
 """
 from serial import Serial
 import serial
+import logging
+from enum import Enum
+
 
 def bit_enabled(byte: bytes, pos: int) -> bool:
-        return int(byte) & (0x01 << pos) != 0
+    return int(byte) & (0x01 << pos) != 0
+
 
 class Status:
 
@@ -112,15 +116,17 @@ class Status:
             "toggle_key": {},
             "system_power": {},
             "external_sensor_connected": {}
-        }}""".format(self.error, self.on, self.preheating, 
-        self.attention_required, self.enabled_pin, self.key_switch, 
-        self.toggle_key, self.system_power, self.external_sensor_connected)    
+        }}""".format(self.error, self.on, self.preheating,
+                     self.attention_required, self.enabled_pin, self.key_switch,
+                     self.toggle_key, self.system_power,
+                     self.external_sensor_connected)
+
 
 class LatchedFailure:
 
     def __init__(self, bytes) -> None:
         """
-        This bit indicates that the laser system is in internal error state 
+        This bit indicates that the laser system is in internal error state
         (safety lockout). This bit is signalized in the “Get Actual Status” 
         value (bit 0), too. 
         """
@@ -136,8 +142,8 @@ class LatchedFailure:
         self.CDRH = bit_enabled(bytes[0], 4)
 
         """
-        A controller<->headcommunication error occurred.With PhoxX lasers this 
-        mostly indicates that the laserhead is not connected correctly to the 
+        A controller<->headcommunication error occurred.With PhoxX lasers this
+        mostly indicates that the laserhead is not connected correctly to the
         controller or the cable is defect.Otherwise this indicates serious 
         electronic problems.
         """
@@ -150,7 +156,7 @@ class LatchedFailure:
         self.k1_relay_error = bit_enabled(bytes[0], 6)
 
         """
-        (Possible with PhoxX lasers only) some diodes of PhoxX lasers need a 
+        (Possible with PhoxX lasers only) some diodes of PhoxX lasers need a
         specially signed “high power controller”.If you own high and low power 
         PhoxX lasers and do mix up the controllers this bit will indicate that
         the actual connected low power controller is not suitable to drive the 
@@ -165,7 +171,7 @@ class LatchedFailure:
         self.under_over_voltage = bit_enabled(bytes[1], 0)
 
         """
-        The external interlock loop was open.(it is still open if this bit is 
+        The external interlock loop was open.(it is still open if this bit is
         set in “Get Failure Byte” command)Note:if the “Auto Reset” function is 
         active this will also be signalized in the “Latched Failure”as long the
         interlock loop is still open, since the device will automatically reset
@@ -205,7 +211,7 @@ class LatchedFailure:
         The diode power exceeded the maximum allowed value.
         """
         self.diode_power = bit_enabled(bytes[1], 7)
-        
+
     def __repr__(self) -> str:
         return """{{
             "error_state": {},
@@ -221,11 +227,13 @@ class LatchedFailure:
             "test_error": {},
             "internal_error": {},
             "diode_power": {}
-        }}""".format(self.error_state, self.CDRH, 
-        self.internal_comunication_error, self.k1_relay_error, self.high_power,
-        self.under_over_voltage, self.external_interlock, self.diode_current, 
-        self.ambient_temp, self.diode_temp, self.test_error, 
-        self.internal_error, self.diode_power)
+        }}""".format(self.error_state, self.CDRH,
+                     self.internal_comunication_error, self.k1_relay_error,
+                     self.high_power, self.under_over_voltage,
+                     self.external_interlock, self.diode_current,
+                     self.ambient_temp, self.diode_temp, self.test_error,
+                     self.internal_error, self.diode_power)
+
 
 class OperationMode:
     def __init__(self, bytes) -> None:
@@ -243,7 +251,7 @@ class OperationMode:
         self.auto_powerup = bit_enabled(bytes[1], 7)
 
     def __repr__(self) -> str:
-        return """{{
+        return """{{ "hex": "{}", "bin": "{}", 
             "internal_clock_generator": {},
             bias_level_release {},
             "operating_level_release", {},
@@ -255,9 +263,10 @@ class OperationMode:
             "usb_adhoc_mode", {},
             "auto_startup", {},
             "auto_powerup", {}
-        }}""".format(self.internal_clock_generator, 
+        }}""".format(hex(int(self)), bin(int(self)),
+                     self.internal_clock_generator,
                      self.bias_level_release,
-                     self.operating_level_release, 
+                     self.operating_level_release,
                      self.digital_input_release,
                      self.analog_input_release,
                      self.APC_mode,
@@ -267,22 +276,40 @@ class OperationMode:
                      self.auto_startup,
                      self.auto_powerup)
 
+    def __int__(self) -> int:
+        return self.internal_clock_generator << 2 | \
+            self.bias_level_release << 3 | \
+            self.operating_level_release << 4 | \
+            self.digital_input_release << 5 | \
+            self.analog_input_release << 7 | \
+            self.APC_mode << 8 | \
+            self.digital_input_impedance << 10 | \
+            self.analog_input_impedance << 12 | \
+            self.usb_adhoc_mode << 13 | \
+            self.auto_startup << 14 | \
+            self.auto_powerup << 15
+
     def __bytes__(self) -> bytes:
-        b = self.internal_clock_generator << 2 | \
-        self.bias_level_release << 3 | \
-        self.operating_level_release << 4 | \
-        self.digital_input_release << 5 | \
-        self.analog_input_release << 7 | \
-        self.APC_mode << 8 | \
-        self.digital_input_impedance << 10 | \
-        self.analog_input_impedance << 12 | \
-        self.usb_adhoc_mode << 13 | \
-        self.auto_startup << 14 | \
-        self.auto_powerup << 15
+        return hex(self.__int__())[2:].encode("Latin1")
 
-        return hex(b)[2:].encode("Latin1")
 
-        
+class CalibrationResult(Enum):
+    SUCCESS = 0
+    MAX_POWER_UNREACHABLE = 1
+    KEY_SWITCH_OFF = 2
+    LASER_ENABLE_INPUT_LOW = 3
+    INTERLOCK_OCURRED = 4
+    DIODE_TEMP_ERROR = 5
+    CONTROLLER_HEAD_COMM_ERROR = 6
+    BIAS_OUT_OF_RANGE = 7
+    NO_BIAS_POINT = 8
+    LESS_THAN_PREV_95 = 9
+    LASER_SWITCHED_OFF = 10
+    NO_CALIBRATION_SENSOR = 11
+    NO_LIGHT_DETECTED = 12
+    OVER_POWER_OCURRED = 13
+    UNKNOWN_ERROR = 14
+
 
 class Omicron_laser:
     """The central Omicron_laser"""
@@ -308,7 +335,7 @@ class Omicron_laser:
             content = decoded[4:].split("|")
             if command.startswith("$TPP"):
                 self.temporal_power = float(content[0])
-            
+
             raw = self._conn.read_until(b'\r')
 
     def __init__(self, conn: Serial):
@@ -326,7 +353,7 @@ class Omicron_laser:
         self.power = specs[1]
 
         self.max_power = self._ask(b"GMP")
-    
+
     def get_working_hours(self):
         return self._ask(b"GWH")[0]
 
@@ -335,7 +362,7 @@ class Omicron_laser:
 
     def measure_temperature_diode(self) -> float:
         return float(self._ask(b"MTD")[0])
-    
+
     def measure_temperature_ambient(self) -> float:
         return float(self._ask(b"MTA")[0])
 
@@ -353,7 +380,7 @@ class Omicron_laser:
     def get_level_power(self):
         response = self._ask(b"GLP")[0]
         return int(response, 16)
-    
+
     def set_level_power(self, value: int) -> bool:
         response = self._set(b"SLP", hex(value)[2:].encode("Latin1"))
         self._process_adhoc()
@@ -374,11 +401,78 @@ class Omicron_laser:
     def update_operation_mode(self):
         mode = bytes(self.operation_mode)
         print("Mode: ", mode)
-        response = self._set(b"SOM", mode)
-        return response
+        response = self._set(b"SOM", mode)[0]
+        return response == '>'
+
+    def set_auto_powerup(self, value: bool) -> bool:
+        response = self._set(b"SAP", str(int(value)).encode("Latin1"))[0]
+        return response == ">"
+
+    def set_auto_startup(self, value: bool) -> bool:
+        response = self._set(b"SAS", str(int(value)).encode("Latin1"))[0]
+        return response == ">"
+
+    def power_on(self) -> bool:
+        response = self._ask(b"POn")[0]
+        return response == ">"
+
+    def power_off(self) -> bool:
+        response = self._ask(b"POf")[0]
+        return response == ">"
+
+    def laser_on(self) -> bool:
+        response = self._ask(b"LOn")[0]
+        return response == ">"
+
+    def laser_off(self) -> bool:
+        response = self._ask(b"LOf")[0]
+        return response == ">"
+
+    def reset(self) -> bool:
+        self._conn.write(b"?RsC\r")
+        response = self._conn.read_until(b'\r')
+        recv = response == b"!RsC\r"
+        logging.info("Reset command received. Laser reponse: {}".format(recv))
+
+        if recv:
+            response = self._conn.read_until(b'\r')
+            while response != b'\x00$RsC>\r':
+                response += self._conn.read_until(b'\r')
+                logging.info(
+                    "Reset in course, Laser response: {}".format(response))
+            return True
+
+        return False
+
+    def set_auto_reset(self, value) -> bool:
+        """
+        Auto reset will not work for class 4 lasers or if a laser system is in 
+        CDRH mode.
+        """
+        response = self._set(b"ARs", str(int(value)).encode("Latin1"))[0]
+        return response == ">"
+
+    def calibrate_laser_diode(self) -> CalibrationResult:
+        response = self._set(b"CLD", b'')[0]
+        if response == ">":
+            logging.info("Laser calibration initiated")
+            response = self._conn.read_until(b'\r')
+            logging.info("Laser GCI: {}".format(response))
+
+            response = self._conn.read_until(b'\r')
+            while b"$CLD" not in response:
+                response += self._conn.read_until(b'\r')
+                print(response)
+                logging.info("Laser calibration in course.")
+
+            return CalibrationResult(int(response[4:-1]))
+
+        return CalibrationResult.UNKNOWN_ERROR
+
 
 if __name__ == "__main__":
     from time import sleep
+    logging.basicConfig(level=logging.INFO)
 
     s = serial.serial_for_url("COM4")
     s.baudrate = 500000
@@ -400,7 +494,7 @@ if __name__ == "__main__":
 
     print("Set Level power to 0xFF0", laser.set_level_power(0xFF0))
     print("Level power: ", laser.get_level_power()/0xFFF)
-    
+
     print("Temporary power: ", laser.get_temporary_power())
     print("Set temporal power", laser.set_temporary_power(0.56))
     print("Temporary power: ", laser.get_temporary_power())
@@ -408,4 +502,20 @@ if __name__ == "__main__":
     op_mode = laser.get_operation_mode()
     print("Laser operation mode:", op_mode)
 
-    laser.update_operation_mode()
+    laser.operation_mode.usb_adhoc_mode = False
+    response = laser.update_operation_mode()
+
+    print("Laser set auto power-up:", laser.set_auto_powerup(True))
+    print("Laser set auto start-up:", laser.set_auto_startup(True))
+
+    print("Power on:", laser.power_on())
+    print("Laser on:", laser.laser_on())
+
+    print("Laser off:", laser.laser_off())
+    print("Power off:", laser.power_off())
+
+    print("Laser set auto reset:", laser.set_auto_reset(True))
+
+    print("Laser calibration:", laser.calibrate_laser_diode().name)
+
+    # laser.reset()
